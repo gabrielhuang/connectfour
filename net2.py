43973815709e9cb97f9c4a2db75185bf6d33fb81
gabi <gabi.xiaohuang@gmail.com>
2015-05-17 14:09:06 -0400

    [ADD] test accuracy

--- net2.py
+++ net2.py
@@ -63,9 +63,9 @@ eta_var = T.scalar()
 #updates = nn.updates.sgd(cost_var, params, learning_rate=eta_var)
 updates = nn.updates.nesterov_momentum(cost_var, params, learning_rate=eta_var)
 
-predict = theano.function([l_in.input_var], [l_out.get_output()])
+predict = theano.function([l_in.input_var], l_out.get_output())
 train = theano.function([l_in.input_var, objective.target_var, eta_var], cost_var, updates=updates)
-get_cost = theano.function([l_in.input_var, objective.target_var], cost_var)
+get_cost = theano.function([l_in.input_var, objective.target_var], [cost_var, l_out.get_output()])
 #%%
 
 '''
@@ -99,6 +99,7 @@ epochs = 10000
 eta = 0.1
 train_costs = []
 test_costs = []
+test_accuracy = []
 batch_size = 20
 for epoch in xrange(epochs):
     # REsample trainset
@@ -115,9 +116,13 @@ for epoch in xrange(epochs):
         batch_costs.append(batch_cost)
     
     train_cost = np.mean(batch_costs)
-    test_cost = get_cost(X_test, y_test)
-    test_costs.append(test_cost)    
+    test_cost, y_pred = get_cost(X_test, y_test)
+    accuracy = ((y_pred>0.5)==y_test).all(axis=1).mean()
+    
+    test_costs.append(test_cost)
+    test_accuracy.append(accuracy)
     
     print 'Epoch {}'.format(epoch)
     print 'Train cost {}'.format(train_cost)
     print 'Test cost {}'.format(test_cost)
+    print 'Test accuracy {}'.format(accuracy)
